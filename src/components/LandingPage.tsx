41b25dc3ddd5b638d6810bac91c457573cffcc68
Lara_Click-at-287693801446 <lara.ortiz@myhnt.com.ar>
2024-12-16 16:42:23 -0300

    feature/background

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,29 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div className="blur-background">
+      <div className="content">
+     </div>
+
+<footer className="footer">
+  <a href="https://www.linkedin.com/in/lara-ortiz-b5b56b1a8/" 
+     className="footer-link" 
+     target="_blank" 
+     rel="noopener noreferrer">
+    Privacy Policy
+  </a>
+  <span className="separator">|</span>
+  <a href="https://www.linkedin.com/in/lara-ortiz-b5b56b1a8/" 
+     className="footer-link" 
+     target="_blank" 
+     rel="noopener noreferrer">
+    Terms of Use
+  </a>
+</footer>
+
+    </div>
+  );
+};
+
+export default LandingPage;
